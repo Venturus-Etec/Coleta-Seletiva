1844a7404ee9f2a161fce5d709a850fe3f088af5
Matheus0192072 <matheusshenrique019207@gmail.com>
2025-09-05 17:37:22 -0300

    mudando o tempo do carrossel e corrigindo o botão do mapa

--- Projeto 'Coleta-Seletiva'/JS/main.js
+++ Projeto 'Coleta-Seletiva'/JS/main.js
@@ -11,9 +11,50 @@ function carrossel(){
     imgs.style.transform = `translateX(${-idex * 100}vw)`;
 }
 
-setInterval(carrossel, 1000);
+setInterval(carrossel, 4000);
 
 function MudaTela (){
     window.location.href = "PgPontos.html"
 }
+
+function Mostrarmap() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          const lat = pos.coords.latitude;
+          const lon = pos.coords.longitude;
+
+          const map = L.map('map').setView([lat, lon], 13);
+          const icon = new L.Icon({
+            iconUrl: 'IMG/lixeira-removebg-preview.png',
+            iconSize: [38, 38],
+            iconAnchor: [16, 32],
+            popupAnchor: [0, -32],
+            shadowUrl: null,
+          });
+
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+          }).addTo(map);
+
+          L.marker([-21.911418445393206, -49.89756946618182], { icon: icon }).addTo(map)
+            .bindPopup("Ponto de Coleta Seletiva")
+
+          L.marker([-21.686356474595634, -49.75168074036299], { icon: icon }).addTo(map)
+            .bindPopup("Ponto de Coleta Seletiva")
+
+          L.marker([-21.649110717229846, -49.738585918570706], { icon: icon }).addTo(map)
+            .bindPopup("Ponto de Coleta Seletiva")
+
+          L.marker([-21.661703910168615, -49.75310489599966], { icon: icon }).addTo(map)
+            .bindPopup("Ponto de Coleta Seletiva")
+
+          L.marker([lat, lon]).addTo(map)
+            .bindPopup("Você está aqui!")
+            .openPopup();
+        });
+      } else {
+        alert("Seu navegador não suporta geolocalização.");
+      }
+    }
+
 
