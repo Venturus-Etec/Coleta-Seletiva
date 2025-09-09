2d4892461d3425c36db848b2747914f7b057b205
Matheus0192072 <matheusshenrique019207@gmail.com>
2025-09-09 02:28:09 -0300

    adicionando mais pontos de coleta e ajustando o mapa

--- Projeto 'Coleta-Seletiva'/JS/main.js
+++ Projeto 'Coleta-Seletiva'/JS/main.js
@@ -31,7 +31,8 @@ function Mostrarmap() {
           });
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19
+          maxZoom: 19,
+          minZoom: 8
         }).addTo(map);
 
         const marker = L.marker([lat, lon]).addTo(map);
@@ -42,7 +43,18 @@ function Mostrarmap() {
           { lat: -21.911418445393206, lon: -49.89756946618182, nome: "Ponto De Coleta" },
           { lat: -21.686356474595634, lon: -49.75168074036299, nome: "Ponto De Coleta" },
           { lat: -21.649110717229846, lon: -49.738585918570706, nome: "Ponto De Coleta" },
-          { lat: -21.661703910168615, lon: -49.75310489599966, nome: "Ponto De Coleta" }
+          { lat: -21.661703910168615, lon: -49.75310489599966, nome: "Ponto De Coleta" },
+          { lat: -22.192484308999113, lon: -49.95718676405406, nome: "Ponto De Coleta" },
+          { lat: -22.218403366264127, lon: -49.9388512414958, nome: "Ponto De Coleta" },
+          { lat: -22.252182784232936, lon: -49.925523468475006, nome: "Ponto De Coleta" },
+          { lat: -21.800285027454688, lon: -49.92715971054123, nome: "Ponto De Coleta" },
+          { lat: -22.010182666466108, lon: -49.78641708272278, nome: "Ponto De Coleta" },
+          { lat: -21.894264624201643, lon: -49.58571685873648, nome: "Ponto De Coleta" },
+          { lat: -21.80440750421986,  lon: -49.601097969947034, nome: "Ponto De Coleta" },
+          { lat: -22.100316552771524, lon: -50.1829166917781, nome: "Ponto De Coleta" },
+          { lat: -21.619793156214293, lon: -49.79976107048435, nome: "Ponto De Coleta" },
+          { lat: -21.54023450106248,  lon: -49.857055386636844, nome: "Ponto De Coleta" },
+
         ];
 
         pontosColeta.forEach((ponto, index) => {
