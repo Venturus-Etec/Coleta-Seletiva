257b018357a0dc31d6b9fad088ecbb1b7478fe50
Matheus <matheusshenrique019207@gmail.com>
2025-11-08 23:41:56 -0300

    efeito no carrosel

--- JS/carrosel.js
+++ JS/carrosel.js
@@ -4,10 +4,12 @@ const img = document.querySelectorAll('#img1 img');
 let idex = 0 
 
 function carrossel(){
+    img[idex].classList.remove('active');
     idex++;
     if(idex > img.length -1){
         idex = 0;
     }
+    img[idex].classList.add('active');
     imgs.style.transform = `translateX(${-idex * 100}%)`;
 }
 
